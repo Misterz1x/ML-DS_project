fd82c1845c043dcf327f88de8df7de610d03b1b8
Misterz1x <ze8907@mci4me.at>
2025-01-02 18:42:26 +0100

    worked on the jupiter notebook. This is a version that could be handed in

--- src/spam_filter/filter.py
+++ src/spam_filter/filter.py
@@ -1,39 +1,134 @@
-import numpy as np
 import pandas as pd
-from email.parser import Parser
-from email import message_from_string
-import re
+import numpy as np
+import os
+import email
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
+
+
+def extract_email(file_path):
+    """
+    Read each email in the directory & parse email info
+    Path: Variable used to define folder location
+    """
+    with open(file_path, 'rb') as f:
+        msg = email.message_from_bytes(f.read())
+        
+    # Get email headers
+    headers = msg.get('Headers')
+    
+    # Read email’s body
+    body = str(msg.get_payload())
+    
+    # Check if body contains HTML tags
+    if '<' in body and '>' in body:
+        # Remove HTML tags if it's valid markup
+        body = BeautifulSoup(body, features='html.parser').get_text()
+    return headers, body
+
+
+ham_path = './dataset/easy_ham'
+spam_path = './dataset/spam'
+
+ham = []
+spam = []
+
+def email_to_dataframe(directory_path, lst):
+    for file in os.listdir(directory_path):
+        file_path = f"{directory_path}/{file}"
+        X = extract_email(file_path)
+        lst.append(X)
+
+email_to_dataframe(ham_path, ham)
+email_to_dataframe(spam_path, spam)
+
+ham_0 = pd.Series(np.zeros(len(ham)))
+spam_1 = pd.Series(np.ones(len(spam)))
+
+df_ham = pd.DataFrame()
+df_ham['email'] = ham
+df_ham['spam (1) or ham (0)'] = ham_0
+
+df_spam = pd.DataFrame()
+df_spam['email'] = spam
+df_spam['spam (1) or ham (0)'] = spam_1
+
+df = pd.concat([df_ham, df_spam], ignore_index=True)
+df['email'] = df['email'].astype(str)
+df['spam (1) or ham (0)'] = df['spam (1) or ham (0)'].astype(int)
+df['email'] = df['email'].str.replace(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', regex=True)
+
+
+print(df.head())
 
 
 
-one_file = ('./dataset/easy_ham/0001.ea7e79d3153e7469e7a9c3e0af6a357e')
+tf_idf = TfidfVectorizer(stop_words='english')
 
-email_message = message_from_string(open(one_file).read())
+bow = tf_idf.fit_transform(df['email']).toarray()
 
-# Extract the email body
-if email_message.is_multipart():
-    # If multipart, iterate through parts
-    for part in email_message.walk():
-        # Only extract text/plain or text/html content
-        if part.get_content_type() == "text/plain":
-            email_text = part.get_payload(decode=True).decode(part.get_content_charset())
-            break
-else:
-    # For non-multipart emails
-    email_text = email_message.get_payload(decode=True).decode(email_message.get_content_charset())
+vect = pd.DataFrame(bow, columns=tf_idf.get_feature_names())
+vect["Spam (1) or ham (0)"] = df["Spam (1) or ham (0)"]
 
 
-# Use regular expressions to change the email text
+X_train, X_test, y_train, y_test = train_test_split(vect.iloc[:,:-1], vect['Spam (1) or ham (0)'], test_size = 0.25)
+
+
+clf = SGDClassifier()
+
+clf.fit(X_train, y_train)
+y_pred_train = clf.predict(X_train)
+y_pred_test = clf.predict(X_test)
+
+conf_mat_train = confusion_matrix(y_train, y_pred_train)
+conf_mat_test = confusion_matrix(y_test, y_pred_test)
+
+results = pd.DataFrame(index = None)
+results['Metric'] = ['Precision', 'Recall', 'Accuracy']
+results['Classifier Train'] = [(precision_score(y_train, y_pred_train)), (recall_score(y_train, y_pred_train)), (clf.score(X_train, y_train))]
+results['Classifier Test'] = [(precision_score(y_test, y_pred_test)), (recall_score(y_test, y_pred_test)), (clf.score(X_test, y_test))]
+
+print(results.to_string(index=False))
+
+
+def confusion_matrix_plot(conf_mat):
+    n = len(conf_mat)
+    plt.imshow(conf_mat, cmap='Blues', extent=[-0.5, n-0.5, -0.5, n-0.5])
+    
+    for i in range(n):
+        for j in range(n):
+            plt.text(i, j, conf_mat[n-j-1, i], ha='center', va='center')
+    
+    plt.colorbar()
+    plt.xticks(range(n))
+    plt.yticks(range(n), range(n-1, -1, -1))
+    plt.xlabel('Predicted Class')
+    plt.ylabel('True Class')
+    plt.plot
 
 
 
+def plot_learning_curve(test_error, training_size):
+    plt.plot(training_size, test_error)
+    plt.title("Learning Curve")
+    plt.xlabel("Training Size")
+    plt.ylabel("Test Accuracy")
+    plt.plot
 
-# Save to a text file
-file_path = "email_body.txt"
-with open(file_path, "w") as file:
-    file.write(email_text)
+
+print("Train Confusion Matrix:")
+confusion_matrix_plot(conf_mat_train)
 
 
-
-#sample_email = open(one_file, 'r').read()   # read the file
-#print(sample_email)
+print("Test Confusion Matrix:")
+confusion_matrix_plot(conf_mat_test)
